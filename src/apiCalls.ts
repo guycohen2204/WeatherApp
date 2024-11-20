4a44e128db336fd2b65e4b1e629e21af5cf6babd
guycohen2204 <guyco@wix.com>
2024-11-20 11:47:45 +0200

    handling api errors

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,18 +1,36 @@
 const apiKey = 'fdf942f263cc4c66ae7120239241111';
-const endpoint = 'http://api.weatherapi.com/v1';
+const endpoint = 'https://api.weatherapi.com/v1';
 
 const getCurrentWeatherByCity = async (cityName: string) => {
     try {
-        const response = await fetch(`${endpoint}/current.json?key=${apiKey}&q=${cityName}`);
-    } catch (error) {
+        const response = await fetch(
+            `${endpoint}/current.json?key=${apiKey}&q=${cityName}`
+        );
 
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error);
     }
-}
+};
 
 const getForecastByCity = async (cityName: string, days: number) => {
     try {
-        const response = await fetch(`${endpoint}/forecast.json?key=${apiKey}&q=${cityName}&days=${days}`);
+        const response = await fetch(
+            `${endpoint}/forecast.json?key=${apiKey}&q=${cityName}&days=${days}`
+        );
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json;
     } catch (error) {
-        
+        console.error(error);
     }
-}
+};
